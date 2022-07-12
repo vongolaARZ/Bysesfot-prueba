9b88a3c252d30a3d509b73d49d54de3e7adb7831
vongolARZ <alderetefacugg@gmial.com>
2022-07-12 13:03:06 -0300

    wq

--- src/main/java/com/bysesoft/bysesoft/common/handler/AbstractExceptionHandler.java
+++ src/main/java/com/bysesoft/bysesoft/common/handler/AbstractExceptionHandler.java
@@ -8,12 +8,8 @@ import org.apache.catalina.connector.ClientAbortException;
 import org.springframework.beans.ConversionNotSupportedException;
 import org.springframework.beans.TypeMismatchException;
 import org.springframework.http.*;
-import org.springframework.http.converter.HttpMessageNotReadableException;
-import org.springframework.http.converter.HttpMessageNotWritableException;
 import org.springframework.lang.Nullable;
 import org.springframework.util.CollectionUtils;
-import org.springframework.web.HttpMediaTypeNotAcceptableException;
-import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
 import org.springframework.web.bind.MissingPathVariableException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
@@ -21,8 +17,6 @@ import org.springframework.web.bind.ServletRequestBindingException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.ResourceAccessException;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.context.request.async.AsyncRequestTimeoutException;
 import org.springframework.web.multipart.support.MissingServletRequestPartException;
@@ -32,7 +26,6 @@ import javax.servlet.ServletException;
 import javax.validation.ConstraintViolationException;
 import java.net.BindException;
 import java.net.ProtocolException;
-import java.net.SocketTimeoutException;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -41,15 +34,11 @@ import java.util.stream.Collectors;
 public abstract sealed class AbstractExceptionHandler permits GlobalExceptionHandler {
 
     @ExceptionHandler({HttpRequestMethodNotSupportedException.class,
-            HttpMediaTypeNotSupportedException.class,
-            HttpMediaTypeNotAcceptableException.class,
             MissingPathVariableException.class,
             MissingServletRequestParameterException.class,
             ServletRequestBindingException.class,
             ConversionNotSupportedException.class,
             TypeMismatchException.class,
-            HttpMessageNotReadableException.class,
-            HttpMessageNotWritableException.class,
             MissingServletRequestPartException.class,
             BindException.class,
             ConstraintViolationException.class,
@@ -69,33 +58,15 @@ public abstract sealed class AbstractExceptionHandler permits GlobalExceptionHan
         if (throwable instanceof HttpRequestMethodNotSupportedException e) {
             status = HttpStatus.METHOD_NOT_ALLOWED;
             return this.handleHttpRequestMethodNotSupported(e, headers, status, request);
-        } else if (throwable instanceof HttpMediaTypeNotSupportedException e) {
-            status = HttpStatus.UNSUPPORTED_MEDIA_TYPE;
-            return this.handleHttpMediaTypeNotSupported(e, headers, status, request);
-        } else if (throwable instanceof HttpMediaTypeNotAcceptableException e) {
-            status = HttpStatus.NOT_ACCEPTABLE;
-            return this.handleHttpMediaTypeNotAcceptable(e, headers, status, request);
         } else if (throwable instanceof MissingPathVariableException e) {
             status = HttpStatus.INTERNAL_SERVER_ERROR;
             return this.handleMissingPathVariable(e, headers, status, request);
         } else if (throwable instanceof MissingServletRequestParameterException e) {
             status = HttpStatus.BAD_REQUEST;
             return this.handleMissingServletRequestParameter(e, headers, status, request);
-        } else if (throwable instanceof ServletRequestBindingException e) {
-            status = HttpStatus.BAD_REQUEST;
-            return this.handleServletRequestBindingException(e, headers, status, request);
-        } else if (throwable instanceof ConversionNotSupportedException e) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR;
-            return this.handleConversionNotSupported(e, headers, status, request);
         } else if (throwable instanceof TypeMismatchException e) {
             status = HttpStatus.BAD_REQUEST;
             return this.handleTypeMismatch(e, headers, status, request);
-        } else if (throwable instanceof HttpMessageNotReadableException e) {
-            status = HttpStatus.BAD_REQUEST;
-            return this.handleHttpMessageNotReadable(e, headers, status, request);
-        } else if (throwable instanceof HttpMessageNotWritableException e) {
-            status = HttpStatus.INTERNAL_SERVER_ERROR;
-            return this.handleHttpMessageNotWritable(e, headers, status, request);
         } else if (throwable instanceof IllegalArgumentException e) {
             status = HttpStatus.BAD_REQUEST;
             return this.handleIllegalArgumentException(e, headers, status, request);
@@ -107,11 +78,6 @@ public abstract sealed class AbstractExceptionHandler permits GlobalExceptionHan
         } else if (throwable instanceof NoHandlerFoundException e) {
             status = HttpStatus.NOT_FOUND;
             return this.handleNoHandlerFoundException(e, headers, status, request);
-        } else if (throwable instanceof AsyncRequestTimeoutException || throwable instanceof ProtocolException
-                || throwable instanceof ServletException || throwable instanceof ClientAbortException
-                || throwable instanceof SocketTimeoutException || throwable instanceof RestClientException) {
-            status = HttpStatus.SERVICE_UNAVAILABLE;
-            return this.handleServiceUnavailableException((Exception) throwable, headers, status, request);
         } else if (throwable instanceof ConstraintViolationException e) {
             return this.handleConstraintViolationException(e, headers);
         } else {
@@ -132,33 +98,6 @@ public abstract sealed class AbstractExceptionHandler permits GlobalExceptionHan
         return this.handleExceptionInternal(ex, headers, status, request, ErrorCode.METHOD_NOT_CURRENTLY_ALLOWED, message);
     }
 
-    protected ResponseEntity<Object> handleHttpMediaTypeNotSupported(HttpMediaTypeNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        if (log.isDebugEnabled())
-            log.debug("handling HttpMediaTypeNotSupported with status={}", status);
-        List<MediaType> mediaTypes = ex.getSupportedMediaTypes();
-        if (!CollectionUtils.isEmpty(mediaTypes)) {
-            headers.setAccept(mediaTypes);
-            if (request instanceof ServletWebRequest servletWebRequest) {
-                if (HttpMethod.PATCH.equals(servletWebRequest.getHttpMethod())) {
-                    headers.setAcceptPatch(mediaTypes);
-                }
-            }
-        }
-
-        final String message = "%s Supported media-types [%s]."
-                .formatted(ErrorCode.MEDIA_TYPE_NOT_SUPPORTED.getDefaultMessage(), ex.getSupportedMediaTypes());
-        return this.handleExceptionInternal(ex, headers, status, request, ErrorCode.MEDIA_TYPE_NOT_SUPPORTED, message);
-    }
-
-    protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        if (log.isDebugEnabled())
-            log.debug("handling HttpMediaTypeNotAcceptable with status={}", status);
-
-        final String message = "%s Allowed media-types [%s]."
-                .formatted(ErrorCode.MEDIA_TYPE_NOT_CURRENTLY_ALLOWED.getDefaultMessage(), ex.getSupportedMediaTypes());
-        return this.handleExceptionInternal(ex, headers, status, request, ErrorCode.MEDIA_TYPE_NOT_CURRENTLY_ALLOWED, message);
-    }
-
     protected ResponseEntity<Object> handleMissingPathVariable(MissingPathVariableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         if (log.isDebugEnabled())
             log.debug("handling eMissingPathVariable with status={}", status);
@@ -175,23 +114,6 @@ public abstract sealed class AbstractExceptionHandler permits GlobalExceptionHan
                 .formatted(ErrorCode.MISSING_REQUEST_PARAMETERS.getDefaultMessage(), ex.getParameterName(), ex.getParameterType());
         return this.handleExceptionInternal(ex, headers, status, request, ErrorCode.MISSING_REQUEST_PARAMETERS, message);
     }
-
-    protected ResponseEntity<Object> handleServletRequestBindingException(ServletRequestBindingException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        if (log.isDebugEnabled())
-            log.debug("handling ServletRequestBinding with status={}", status);
-
-        return this.handleExceptionInternal(ex, headers, status, request, ErrorCode.REQUEST_BINDING, null);
-    }
-
-    protected ResponseEntity<Object> handleConversionNotSupported(ConversionNotSupportedException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        if (log.isDebugEnabled())
-            log.debug("handling ConversionNotSupported with status={}", status);
-
-        final String message = "%s The offending value [%s] couldn't be converted."
-                .formatted(ErrorCode.CONVERTION_NOT_SUPPORTED.getDefaultMessage(), ex.getValue());
-        return this.handleExceptionInternal(ex, headers, status, request, ErrorCode.CONVERTION_NOT_SUPPORTED, message);
-    }
-
     protected ResponseEntity<Object> handleTypeMismatch(TypeMismatchException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         if (log.isDebugEnabled())
             log.debug("handling handleTypeMismatch with status={}", status);
@@ -201,19 +123,8 @@ public abstract sealed class AbstractExceptionHandler permits GlobalExceptionHan
         return this.handleExceptionInternal(ex, headers, status, request, ErrorCode.TYPE_MISMATCH, message);
     }
 
-    protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        if (log.isDebugEnabled())
-            log.debug("handling HttpMessageNotReadable with status={}", status);
 
-        return this.handleExceptionInternal(ex, headers, status, request, ErrorCode.MESSAGE_NOT_READABLE, null);
-    }
 
-    protected ResponseEntity<Object> handleHttpMessageNotWritable(HttpMessageNotWritableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        if (log.isDebugEnabled())
-            log.debug("handling HttpMessageNotWritable with status={}", status);
-
-        return this.handleExceptionInternal(ex, headers, status, request, ErrorCode.MESSAGE_NOT_WRITABLE, null);
-    }
 
     protected ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         if (log.isDebugEnabled())
@@ -249,12 +160,6 @@ public abstract sealed class AbstractExceptionHandler permits GlobalExceptionHan
         return this.handleExceptionInternal(ex, headers, status, request, ErrorCode.NOT_HANDLER_FOUND, message);
     }
 
-    protected ResponseEntity<Object> handleServiceUnavailableException(Exception ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        if (log.isDebugEnabled())
-            log.debug("handling ServiceUnavailable with status={}", status);
-
-        return this.handleExceptionInternal(ex, headers, status, request, ErrorCode.SERVICE_UNAVAILABLE, null);
-    }
 
     protected ResponseEntity<Object> handleExceptionInternal(Exception ex, HttpHeaders headers, HttpStatus status, WebRequest request,
                                                              ErrorCode code, @Nullable String customMessage) {
--- src/main/java/com/bysesoft/bysesoft/ports/imputs/rest/dtos/CategoryDto.java
+++ src/main/java/com/bysesoft/bysesoft/ports/imputs/rest/dtos/CategoryDto.java
@@ -7,7 +7,6 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 @Getter
 @Setter
